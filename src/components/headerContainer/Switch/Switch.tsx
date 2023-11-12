4a78782d839d8e88dc86338898a0d0b2be311ad6
Andrew <ak36794ca@gmail.com>
2023-11-12 09:28:29 +0200

    add storedTheme in localStoreage

--- src/components/headerContainer/Switch/Switch.tsx
+++ src/components/headerContainer/Switch/Switch.tsx
@@ -3,26 +3,28 @@ import React, {FC, useEffect, useState} from 'react';
 import css from "./Switch.module.css"
 
 type ChangeEvent = React.ChangeEvent<HTMLInputElement>
-type Theme = 'dark'|'light'
 
 const Switch: FC = () => {
+    const storedTheme: string = localStorage.getItem('theme');
+    const [theme, setTheme] = useState(storedTheme ? storedTheme : 'light');
 
-    const [theme, setTheme] = useState<Theme>('light');
+    const handleChange = (e: ChangeEvent) => {
+        setTheme(e.target.checked ? 'dark' : 'light');
+        localStorage.setItem("theme", e.target.checked ? 'dark' : 'light');
+    };
 
-    const handleChange=(e:ChangeEvent)=>setTheme(e.target.checked?'dark':'light');
 
     useEffect(() => {
-        document.body.setAttribute('data-theme',theme);
-    }, [theme]);
+        document.body.setAttribute('data-theme', theme);
+    }, [theme, storedTheme]);
 
     return (
         <div className={css.container_switch}>
             <div className={css.title}>Change Theme</div>
             <label className={css.switch}>
-                <input  type="checkbox" onChange={handleChange} checked={theme === 'dark'}/>
+                <input type="checkbox" onChange={handleChange} checked={theme === 'dark'}/>
                 <span className={css.slider}></span>
             </label>
-
         </div>
     );
 };
